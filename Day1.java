c45c9ecf4046485cca2d8b2a992dc9750a86966b
Ulima Inas Shabrina <44187412+shabrinaiu@users.noreply.github.com>
2020-10-23 23:07:35 +0700

    Create Day1.java
    Java Syntax, Comments, Variables and Data Types

---
+++ Day1.java
@@ -0,0 +1,17 @@
+
+public class MyClass {
+  public static void main(String[] args) {
+    float myNum = 5.75f;
+    char a = 65, b = 66, c = 67;
+    int myNum = 5;               // Integer (whole number)
+    float myFloatNum = 5.99f;    // Floating point number
+    char myLetter = 'D';         // Character
+    boolean myBool = true;       // Boolean
+    String myText = "Hello";     // String
+    
+    System.out.println(myNum);  
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+  }
+}
